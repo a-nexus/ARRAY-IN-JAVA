aa37d13ff32633407a077269c76bb0d4d2c14848
a-nexus <anuragsingh77077@gmail.com>
2026-03-04 09:30:42 +0530

    ARRAY INTIALIZATION IN DYNEMIC WAY

--- Array.java
+++ Array.java
@@ -2,15 +2,23 @@ import java.util.*;
 public class Array{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String[] arr=new String[100];//INITILIZATION OF A ARRAY
-        for(int i=0;i<2;i++){
-            System.out.print("ENTER YOUR NAME:");
-         arr[i]=sc.nextLine();
+        int[] arr=new int[100];//INITILIZATION OF A ARRAY
+        int num;
+        System.out.print("ENTER THE NUMBER OF ELEMENT BETWEEN 0 AND 100:");
+        num=sc.nextInt();
+        if(num>99 || num<=0){
+            System.out.println("INVALID");
+            return ;
         }
-        for(int i=0;i<2;i++){
-            System.out.println(arr[i]+"\t");
+        for(int i=0;i<num;i++){
+            System.out.print("ENTER YOUR NUM:");
+         arr[i]=sc.nextInt();
         }
-
+        for(int i=0;i<num;i++){
+            System.out.print(arr[i]+"\t");
+        }
+return ;
     }
+    
 
 }
