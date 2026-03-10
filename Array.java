f75827c1baf8f5dc59a17227f72f2efd3da79281
a-nexus <anuragsingh77077@gmail.com>
2026-03-10 10:12:48 +0530

    ARRAY WITH LENEAR SEARCH

--- Array.java
+++ Array.java
@@ -1,24 +1,52 @@
-import java.util.*;
-public class Array{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int[] arr=new int[100];//INITILIZATION OF A ARRAY
-        int num;
-        System.out.print("ENTER THE NUMBER OF ELEMENT BETWEEN 0 AND 100:");
-        num=sc.nextInt();
-        if(num>99 || num<=0){
-            System.out.println("INVALID");
-            return ;
-        }
-        for(int i=0;i<num;i++){
-            System.out.print("ENTER YOUR NUM:");
-         arr[i]=sc.nextInt();
-        }
-        for(int i=0;i<num;i++){
-            System.out.print(arr[i]+"\t");
-        }
-return ;
-    }
+// import java.util.*;
+// public class Array{
+//     public static int main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int[] arr=new int[100];//INITILIZATION OF A ARRAY
+//         int num;
+//         System.out.print("ENTER THE NUMBER OF ELEMENT BETWEEN 0 AND 100:");
+//         num=sc.nextInt();
+//         if(num>99 || num<=0){
+//             System.out.println("INVALID");
+//             return 0 ;
+//         }
+//         for(int i=0;i<num;i++){
+//             System.out.print("ENTER YOUR NUM:");
+//          arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<num;i++){
+//             System.out.print(arr[i]+"\t");
+//         }
+// return 0;
+//     }
     
 
+// }
+
+
+//OUESTION 
+//TAKE AN ARRAY AS INPUT FROM THE USER. SEARCH FOR A GIVEN NUMBER X AND PRINT THE INDEX AT WHICH ITS OCCUR
+import java.util.*;
+public class Array{
+    public static void binarySearch(int element,int target){
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[element];
+        for(int i=0;i<element;i++){
+            System.out.print("ENTER THE "+(i+1)+" VALUE=");
+            arr[i]=sc.nextInt();
+        }
+ for(int i=0;i<element;i++){
+    if(arr[i]==target){
+        System.out.print("AT "+i+" INDEX\n");
+    }
+ }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF ELEMENT:");
+        int element=sc.nextInt();
+        System.out.print("ENTER THE TARGETED VALUE:");
+        int target=sc.nextInt();
+         binarySearch(element,target);
+    }
 }
