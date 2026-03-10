723b458e21e35b9daffcb652aa8a500daa0f8ead
a-nexus <anuragsingh77077@gmail.com>
2026-03-10 19:56:29 +0530

    ARRAY SHORTING

--- Array.java
+++ Array.java
@@ -55,20 +55,57 @@
 
 //TAKE AN ARRAY OF NAMES AS INPUT FROM THE USER AND PRINT THEM ON THE SCREEN.
 
+// import java.util.*;
+// public class Array{
+// public static void main(String[] args){
+//     Scanner sc=new Scanner(System.in);
+//     System.out.print("ENTER THE NUMBVER OF NAMES:");
+//     int num=sc.nextInt();
+//     sc.nextLine();
+//     String[] name=new String[num];
+//     for(int i=0;i<name.length;i++){
+//         System.out.print("ENTER THE "+(i+1)+" NAME=");
+//         name[i]=sc.nextLine();
+//     }
+//     for(int i=0;i<name.length;i++){
+//         System.out.println((i+1)+" NAME="+ name[i]);
+//     }
+// }
+// }
+
+
+// //3. TAKE AN ARRAY OF NUMBERS AS INPUT AND CHECK IF IT IS AN ARRAY SORTED IN 
+// ASCENDING ORDER. 
+// EG : { 1, 2, 4, 7 } IS SORTED IN ASCENDING ORDER. 
+//        {3, 4, 6, 2} IS NOT SORTED IN ASCENDING ORDER. 
+
 import java.util.*;
 public class Array{
-public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    System.out.print("ENTER THE NUMBVER OF NAMES:");
-    int num=sc.nextInt();
-    sc.nextLine();
-    String[] name=new String[num];
-    for(int i=0;i<name.length;i++){
-        System.out.print("ENTER THE "+(i+1)+" NAME=");
-        name[i]=sc.nextLine();
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int temp;
+        System.out.print("ENTER THE NUMBER OF ELEMENT:");
+        int element=sc.nextInt();
+        int[] arr=new int[1000];
+        if(element>=1000 || element<=0){
+            System.out.print("INVALID ELEMENT FILLUP");
+            return ;
+        }
+        for(int i=0;i<element;i++){
+            System.out.print("ENTER THE "+(i+1)+" INDEXED VALUE=");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<element;i++){
+            for(int j=i+1;j<element;j++){
+                if(arr[i]>arr[j]){
+                  temp=arr[i];
+                  arr[i]=arr[j];
+                  arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<element;i++){
+            System.out.print(arr[i]+"\t");
+        }
     }
-    for(int i=0;i<name.length;i++){
-        System.out.println((i+1)+" NAME="+ name[i]);
-    }
-}
 }
